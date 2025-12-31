425c58f12fdc6d8067c9a9e431b4cd5e94ec4755
mxy <mengxyokok@gmail.com>
2025-12-31 10:54:31 +0800

    update default parameter to run directly

--- cleanrl/ppo.py
+++ cleanrl/ppo.py
@@ -24,15 +24,15 @@ class Args:
     """if toggled, `torch.backends.cudnn.deterministic=False`"""
     cuda: bool = True
     """if toggled, cuda will be enabled by default"""
-    track: bool = False
+    track: bool = True
     """if toggled, this experiment will be tracked with Weights and Biases"""
     wandb_project_name: str = "cleanRL"
     """the wandb's project name"""
-    wandb_entity: str = None
+    wandb_entity: str = "mengxiaoye"
     """the entity (team) of wandb's project"""
     capture_video: bool = False
     """whether to capture videos of the agent performances (check out `videos` folder)"""
-    render_env: bool = False
+    render_env: bool = True
     """whether to render the environment during training (real-time display)"""
 
     # Algorithm specific arguments
